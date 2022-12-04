ad2a800abccea189e49e2d981c2a7c46fc3e7cfd
Hisham632 <77436699+Hisham632@users.noreply.github.com>
2022-12-04 17:58:19 -0500

    dataGenerated

--- AI model/main.py
+++ AI model/main.py
@@ -246,25 +246,55 @@ def main(vidPath):
     cap.release()
     cv2.destroyAllWindows()
 
-vidPath='IMG_6979-00.00.27.495-00.00.28.872.mp4'
 
-vidpath1 = 'untitled.mp4'
+vidPath= 'data\\proper\\proper[58].mp4'
+
+#vidpath1 =[ '','untitled.mp4']
+
+vidpath1=['','data\\proper\\6976[0].mp4', 'data\\proper\\6976[10].mp4', 'data\\proper\\6976[11].mp4', 'data\\proper\\6976[12].mp4', 'data\\proper\\6976[13].mp4', 'data\\proper\\6976[14].mp4', 'data\\proper\\6976[15].mp4', 'data\\proper\\6976[16].mp4', 'data\\proper\\6976[18].mp4', 'data\\proper\\6976[19].mp4', 'data\\proper\\6976[1].mp4', 'data\\proper\\6976[20].mp4', 'data\\proper\\6976[21].mp4', 'data\\proper\\6976[22].mp4', 'data\\proper\\6976[23].mp4', 'data\\proper\\6976[24].mp4', 'data\\proper\\6976[25].mp4', 'data\\proper\\6976[26].mp4', 'data\\proper\\6976[27].mp4', 'data\\proper\\6976[28].mp4', 'data\\proper\\6976[29].mp4', 'data\\proper\\6976[2].mp4', 
+'data\\proper\\6976[30].mp4', 'data\\proper\\6976[3].mp4', 'data\\proper\\6976[4].mp4', 'data\\proper\\6976[5].mp4', 'data\\proper\\6976[6].mp4', 'data\\proper\\6976[7].mp4', 'data\\proper\\6976[8].mp4', 'data\\proper\\6976[9].mp4', 'data\\proper\\proper[0].mp4', 'data\\proper\\proper[10].mp4', 'data\\proper\\proper[11].mp4', 'data\\proper\\proper[12].mp4', 'data\\proper\\proper[13].mp4', 'data\\proper\\proper[14].mp4', 'data\\proper\\proper[15].mp4', 'data\\proper\\proper[16].mp4', 'data\\proper\\proper[17].mp4', 'data\\proper\\proper[18].mp4', 'data\\proper\\proper[19].mp4', 'data\\proper\\proper[1].mp4', 'data\\proper\\proper[20].mp4', 'data\\proper\\proper[21].mp4', 'data\\proper\\proper[22].mp4', 'data\\proper\\proper[23].mp4', 'data\\proper\\proper[24].mp4', 'data\\proper\\proper[25].mp4', 'data\\proper\\proper[26].mp4', 'data\\proper\\proper[27].mp4', 'data\\proper\\proper[28].mp4', 'data\\proper\\proper[29].mp4', 'data\\proper\\proper[2].mp4', 'data\\proper\\proper[30].mp4', 'data\\proper\\proper[31].mp4', 'data\\proper\\proper[32].mp4', 'data\\proper\\proper[33].mp4', 'data\\proper\\proper[34].mp4', 'data\\proper\\proper[35].mp4', 'data\\proper\\proper[36].mp4', 'data\\proper\\proper[37].mp4', 'data\\proper\\proper[38].mp4', 'data\\proper\\proper[39].mp4', 'data\\proper\\proper[3].mp4', 'data\\proper\\proper[40].mp4', 'data\\proper\\proper[41].mp4', 'data\\proper\\proper[42].mp4', 'data\\proper\\proper[43].mp4', 'data\\proper\\proper[44].mp4', 'data\\proper\\proper[45].mp4', 'data\\proper\\proper[46].mp4', 'data\\proper\\proper[47].mp4', 'data\\proper\\proper[48].mp4', 'data\\proper\\proper[49].mp4', 'data\\proper\\proper[4].mp4', 'data\\proper\\proper[50].mp4', 'data\\proper\\proper[51].mp4', 'data\\proper\\proper[52].mp4', 'data\\proper\\proper[53].mp4', 'data\\proper\\proper[54].mp4', 'data\\proper\\proper[55].mp4', 'data\\proper\\proper[56].mp4', 
+'data\\proper\\proper[57].mp4', 'data\\proper\\6976[17].mp4', 'data\\proper\\proper[59].mp4', 'data\\proper\\proper[5].mp4', 'data\\proper\\proper[60].mp4', 'data\\proper\\proper[61].mp4', 'data\\proper\\proper[62].mp4', 'data\\proper\\proper[63].mp4', 'data\\proper\\proper[64].mp4', 'data\\proper\\proper[65].mp4', 'data\\proper\\proper[66].mp4', 'data\\proper\\proper[67].mp4', 'data\\proper\\proper[68].mp4', 'data\\proper\\proper[69].mp4', 'data\\proper\\proper[6].mp4', 'data\\proper\\proper[70].mp4', 'data\\proper\\proper[71].mp4', 'data\\proper\\proper[72].mp4', 'data\\proper\\proper[73].mp4', 'data\\proper\\proper[74].mp4', 'data\\proper\\proper[75].mp4', 'data\\proper\\proper[76].mp4', 'data\\proper\\proper[77].mp4', 'data\\proper\\proper[78].mp4', 'data\\proper\\proper[79].mp4', 'data\\proper\\proper[7].mp4', 'data\\proper\\proper[80].mp4', 'data\\proper\\proper[81].mp4', 'data\\proper\\proper[82].mp4', 'data\\proper\\proper[83].mp4', 'data\\proper\\proper[84].mp4', 'data\\proper\\proper[85].mp4', 'data\\proper\\proper[86].mp4', 'data\\proper\\proper[87].mp4', 'data\\proper\\proper[88].mp4', 'data\\proper\\proper[89].mp4', 'data\\proper\\proper[8].mp4', 'data\\proper\\proper[90].mp4', 'data\\proper\\proper[91].mp4', 'data\\proper\\proper[92].mp4', 'data\\proper\\proper[93].mp4', 'data\\proper\\proper[94].mp4', 'data\\proper\\proper[95].mp4', 'data\\proper\\proper[9].mp4']
+
+print(vidpath1[84])
+
 main(vidPath)
 
-dataAngles[0][switch].append(list(range(0, len(dataAngles[1][switch]))))
-switch = 1
+# dataAngles[0][switch].append(list(range(0, len(dataAngles[1][switch]))))#first vid
+dataAngles[0][0].append(list(range(0, len(dataAngles[1][0]))))#first vid
 
-main(vidpath1)
+for num in range(1,(len(vidpath1))):
+    dataAngles[0].append([])
+    dataAngles[1].append([])
+#    switch=num#switch to 2nd vid
+    ratio=0
+    switch+=1
+    print(num)
+    
+    
+    main(vidpath1[num])#takes a list of vids
 
-ratio = len(dataAngles[1][0])/len(dataAngles[1][1])
+    #if( len(dataAngles[1][0])<len(dataAngles[1][num]) ):
 
-dataAngles[0][switch].append(list(range(0, len(dataAngles[1][switch]))))
 
-#print(dataAngles)
+    
+    #else:
+     #   ratio = len(dataAngles[1][num])/len(dataAngles[1][0])
+    
 
-for i in range (0,len(dataAngles[1][1])):
-    dataAngles[0][1][0][i] = dataAngles[0][1][0][i]*ratio
-    #print(dataAngles[1][1][i])
+    dataAngles[0][num].append(list(range(0, len(dataAngles[1][num]))))
+    ratio = len(dataAngles[1][0])/len(dataAngles[1][num])
+        
+    print(len(dataAngles[1][0]),"............",len(dataAngles[1][num]),'....RATIO',ratio)
+        #print(dataAngles)
+    
+    for i in range (0, max(len(dataAngles[1][0]), len(dataAngles[1][num]))     ):
+        #print(i)
+
+        dataAngles[0][num][0][i] = dataAngles[0][num][0][i]*ratio
+            #print(dataAngles[1][1][i])
+
+
+
 
 #dataAngles[0][0][0] all the x coordinates of the first vid
 #dataAngles[1][0] all the y coordinates of the first vid
@@ -272,21 +302,47 @@ import numpy as np
 import pandas as pd
 print(dataAngles[1][0])
 
-combinedFirstVidTo2darray=list(zip(dataAngles[0][0][0], dataAngles[1][0]))
+# dataAngles[0][0][0].append(dataAngles[0][1][0])
 
+# array0=np.append(dataAngles[0][0][0], dataAngles[0][1][0])
+
+# # dataAngles[1][0].append(dataAngles[1][0])
+# # array1 = np.append(dataAngles[1][0], dataAngles[1][1])
+
+# print(array1)
+array0=[]
+array1=[]
+NumOfVids=len(vidpath1)
+#For multiple vids
+for num in range(NumOfVids):
+    array0=np.append(array0, dataAngles[0][num][0])
+    array1=np.append(array1, dataAngles[1][num])
+
+
+
+
+#combinedFirstVidTo2darray=list(zip(dataAngles[0][0][0], dataAngles[1][0]))#HERE for first vid only 
+combinedFirstVidTo2darray=list(zip(array0, array1))#HERE for first vid only 
 numArray=np.array(combinedFirstVidTo2darray, dtype=object)
 
-print(dataAngles[1][0][0])
+# print(dataAngles[1][0][0])
 # print(combinedFirstVidTo2darray)
 df = pd.DataFrame(combinedFirstVidTo2darray)#.stack().rename_axis(['x', 'y'])#.reset_index(name='val')
 
-print(df)
+# print(df)
 df.to_csv("./courses.csv")
 
 
-plt.scatter(dataAngles[0][0],dataAngles[1][0])
-plt.scatter(dataAngles[0][1],dataAngles[1][1])
+# plt.scatter(dataAngles[0][0],dataAngles[1][0])
+#= plt.scatter(dataAngles[0][1],dataAngles[1][1])
+plt.scatter(array0,array1)
+
 plt.show()
 
 
 #reference is first vid, and keep ration for all of em 
+
+"""
+compare vid lenghts to apply appropriate ratio
+
+"""
