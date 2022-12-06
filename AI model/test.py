5829da73f23ee0918408d16e85bca4050eaa216c
Hisham632 <77436699+Hisham632@users.noreply.github.com>
2022-12-06 18:04:26 -0500

    modelUpdate

--- AI model/test.py
+++ AI model/test.py
@@ -1,51 +1,126 @@
-# import os
-# # assign directory
-# directory = 'data\\wrong'
-# stingLIst=[]
-# # iterate over files in
-# # that directory
-# for filename in os.listdir(directory):
-#     f = os.path.join(directory, filename)
-#     # checking if it is a file
-#     if os.path.isfile(f):
-#         stingLIst.append(str(f))
-#         # print(filename)
-# print(stingLIst)
-
-
-
-# vidpath1=['','data\\proper\\6976[0].mp4', 'data\\proper\\6976[10].mp4', 'data\\proper\\6976[11].mp4', 'data\\proper\\6976[12].mp4', 'data\\proper\\6976[13].mp4', 'data\\proper\\6976[14].mp4', 'data\\proper\\6976[15].mp4', 'data\\proper\\6976[16].mp4', 'data\\proper\\6976[18].mp4', 'data\\proper\\6976[19].mp4', 'data\\proper\\6976[1].mp4', 'data\\proper\\6976[20].mp4', 'data\\proper\\6976[21].mp4', 'data\\proper\\6976[22].mp4', 'data\\proper\\6976[23].mp4', 'data\\proper\\6976[24].mp4', 'data\\proper\\6976[25].mp4', 'data\\proper\\6976[26].mp4', 'data\\proper\\6976[27].mp4', 'data\\proper\\6976[28].mp4', 'data\\proper\\6976[29].mp4', 'data\\proper\\6976[2].mp4', 
-# 'data\\proper\\6976[30].mp4', 'data\\proper\\6976[3].mp4', 'data\\proper\\6976[4].mp4', 'data\\proper\\6976[5].mp4', 'data\\proper\\6976[6].mp4', 'data\\proper\\6976[7].mp4', 'data\\proper\\6976[8].mp4', 'data\\proper\\6976[9].mp4', 'data\\proper\\proper[0].mp4', 'data\\proper\\proper[10].mp4', 'data\\proper\\proper[11].mp4', 'data\\proper\\proper[12].mp4', 'data\\proper\\proper[13].mp4', 'data\\proper\\proper[14].mp4', 'data\\proper\\proper[15].mp4', 'data\\proper\\proper[16].mp4', 'data\\proper\\proper[17].mp4', 'data\\proper\\proper[18].mp4', 'data\\proper\\proper[19].mp4', 'data\\proper\\proper[1].mp4', 'data\\proper\\proper[20].mp4', 'data\\proper\\proper[21].mp4', 'data\\proper\\proper[22].mp4', 'data\\proper\\proper[23].mp4', 'data\\proper\\proper[24].mp4', 'data\\proper\\proper[25].mp4', 'data\\proper\\proper[26].mp4', 'data\\proper\\proper[27].mp4', 'data\\proper\\proper[28].mp4', 'data\\proper\\proper[29].mp4', 'data\\proper\\proper[2].mp4', 'data\\proper\\proper[30].mp4', 'data\\proper\\proper[31].mp4', 'data\\proper\\proper[32].mp4', 'data\\proper\\proper[33].mp4', 'data\\proper\\proper[34].mp4', 'data\\proper\\proper[35].mp4', 'data\\proper\\proper[36].mp4', 'data\\proper\\proper[37].mp4', 'data\\proper\\proper[38].mp4', 'data\\proper\\proper[39].mp4', 'data\\proper\\proper[3].mp4', 'data\\proper\\proper[40].mp4', 'data\\proper\\proper[41].mp4', 'data\\proper\\proper[42].mp4', 'data\\proper\\proper[43].mp4', 'data\\proper\\proper[44].mp4', 'data\\proper\\proper[45].mp4', 'data\\proper\\proper[46].mp4', 'data\\proper\\proper[47].mp4', 'data\\proper\\proper[48].mp4', 'data\\proper\\proper[49].mp4', 'data\\proper\\proper[4].mp4', 'data\\proper\\proper[50].mp4', 'data\\proper\\proper[51].mp4', 'data\\proper\\proper[52].mp4', 'data\\proper\\proper[53].mp4', 'data\\proper\\proper[54].mp4', 'data\\proper\\proper[55].mp4', 'data\\proper\\proper[56].mp4', 
-# 'data\\proper\\proper[57].mp4', 'data\\proper\\6976[17].mp4', 'data\\proper\\proper[59].mp4', 'data\\proper\\proper[5].mp4', 'data\\proper\\proper[60].mp4', 'data\\proper\\proper[61].mp4', 'data\\proper\\proper[62].mp4', 'data\\proper\\proper[63].mp4', 'data\\proper\\proper[64].mp4', 'data\\proper\\proper[65].mp4', 'data\\proper\\proper[66].mp4', 'data\\proper\\proper[67].mp4', 'data\\proper\\proper[68].mp4', 'data\\proper\\proper[69].mp4', 'data\\proper\\proper[6].mp4', 'data\\proper\\proper[70].mp4', 'data\\proper\\proper[71].mp4', 'data\\proper\\proper[72].mp4', 'data\\proper\\proper[73].mp4', 'data\\proper\\proper[74].mp4', 'data\\proper\\proper[75].mp4', 'data\\proper\\proper[76].mp4', 'data\\proper\\proper[77].mp4', 'data\\proper\\proper[78].mp4', 'data\\proper\\proper[79].mp4', 'data\\proper\\proper[7].mp4', 'data\\proper\\proper[80].mp4', 'data\\proper\\proper[81].mp4', 'data\\proper\\proper[82].mp4', 'data\\proper\\proper[83].mp4', 'data\\proper\\proper[84].mp4', 'data\\proper\\proper[85].mp4', 'data\\proper\\proper[86].mp4', 'data\\proper\\proper[87].mp4', 'data\\proper\\proper[88].mp4', 'data\\proper\\proper[89].mp4', 'data\\proper\\proper[8].mp4', 'data\\proper\\proper[90].mp4', 'data\\proper\\proper[91].mp4', 'data\\proper\\proper[92].mp4', 'data\\proper\\proper[93].mp4', 'data\\proper\\proper[94].mp4', 'data\\proper\\proper[95].mp4', 'data\\proper\\proper[9].mp4']
-
-# print(len(vidpath1))
-
 import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-# Read the data into a Pandas DataFrame
-df = pd.read_csv('./courses.csv')
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_absolute_error
 
-# Group the data by the "Frames" column
-groups = df.groupby('Frames')
 
-# Initialize the input and output arrays
-input_data = []
-output_data = []
 
-# Iterate over the groups
-for _, group in groups:
-    # Split the data into separate sequences when the "Frames" value is 0
-    if group['Frames'].iloc[0] == 0:
-        # Append the "Angles" column as input data
-        input_data.append(group['Angles'].values)
+from mpl_toolkits import mplot3d
+#import seaborn as sns
+#sns.set(style='darkgrid')
 
-        # Calculate the difference between the current and previous values of the "Angles" column as output data
-        output_data.append(group['Angles'].diff().values[1:])
+# arr2 = np.genfromtxt("labeledReps.csv", delimiter=",")
+# frames_arr = arr2[:,0] #all rows in colm 1 
+# angles_arr = arr2[:,1] #all rows in colm 1 
 
-# Create a new Pandas DataFrame with columns for the input and output data
-data = pd.DataFrame(columns=['Input', 'Output'])
-data['Input'] = input_data
-data['Output'] = output_data
+# X_train, X_test, y_train, y_test = train_test_split(frames_arr,angles_arr,test_size=0.3)
 
-# Write the DataFrame to a CSV file
-data.to_csv('formatted_data.csv', index=False)
+# #plt.scatter(frames_arr,angles_arr)
+# #plt.show()
+# arr2New = np.genfromtxt("wrongForm.csv", delimiter=",")
+# frames_arrNew = arr2New[:,3] #all rows in colm 1 
+# angles_arrNew = arr2New[:,2] #all rows in colm 1 
+
+# X_train2, X_test2, y_train2, y_test2 = train_test_split(frames_arrNew,angles_arrNew,test_size=0.3)
+# x_train_data2 = np.array([X_train2]).reshape(-1,1)
+# #y_train_data = np.array([y_train]).reshape(-1,1)
+# x_test_data2 = np.array([X_test2]).reshape(-1,1)
+# #y_test_data = np.array([y_test]).reshape(-1,1)
+
+df=pd.read_csv("labeledReps.csv")
+print(df.head())
+
+arrayX= df[['Frames', 'Angles']].values
+
+# print(arrayX)
+label=df['label']
+
+X_train, X_test, y_train, y_test = train_test_split(arrayX,label,test_size=0.2)
+
+
+# x_train_data = np.array([X_train]).reshape(-1,1)
+# y_train_data = np.array([y_train]).reshape(-1,1)
+# x_test_data = np.array([X_test]).reshape(-1,1)
+# y_test_data = np.array([y_test]).reshape(-1,1)
+
+
+
+'''
+clf = LinearRegression()
+clf.fit(x_train_data,y_train)
+clf.predict(x_test_data)
+print(clf.score(x_test_data,y_test))
+'''
+
+
+rf = RandomForestRegressor(n_estimators=100, max_depth=2,
+                           min_samples_split=2, random_state=1)
+
+
+
+from sklearn.model_selection import RandomizedSearchCV
+
+model = RandomForestRegressor()
+
+'''
+#Used to find best parameters {'min_samples_split': 28, 'max_depth': 6}
+rs = RandomizedSearchCV(model, n_iter=10,
+                        param_distributions = {'max_depth': range(1, 15),
+                                               'min_samples_split': range(2, 50)},
+                        cv=5, n_jobs=-1, random_state=3,
+                        scoring='neg_mean_squared_error')
+
+#print(rs.best_params_)
+'''
+from sklearn.model_selection import cross_val_score
+
+rf = RandomForestRegressor(n_estimators=100, max_depth=6,
+                           min_samples_split=28, random_state=42)
+
+
+
+# print(X_train)
+rf.fit(X_train, y_train)
+
+pred_y_data = rf.predict(X_test)
+# pred_y_data2 = rf.predict(x_test_data2)
+
+#plt.scatter(X_train, y_train)
+# # plt.scatter(X_test,y_test)
+# print(x_test_data.reshape(-1,1))
+# print(pred_y_data.reshape(-1,1))
+
+# plt.scatter(X_test,pred_y_data)
+print(pred_y_data.shape)
+print(X_test.shape)
+
+# plt.scatter(X_test,pred_y_data)
+
+print('Training Features Shape:', X_train.shape)
+print('Training Labels Shape:', X_test.shape)
+print('Testing Features Shape:', y_train.shape)
+print('Testing Labels Shape:', y_test.shape)
+
+errors = abs(pred_y_data - y_test)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+
+
+scores = cross_val_score(estimator=rf, X=X_test, y=pred_y_data, cv=10,)
+print(scores)
+
+print(rf.score(X_test,pred_y_data))
+#print(mean_absolute_error(y_test, pred_y_data))
+#print(accuracy_score(y_test,pred_y_data))
+
+#print(-rs.best_score_)
+plt.show()
+
