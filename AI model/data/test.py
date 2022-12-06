5a37cc79fac3b6bb3853d24b5f0a2ce562308ec1
Hisham632 <77436699+Hisham632@users.noreply.github.com>
2022-12-05 23:35:59 -0500

    initModel

--- AI model/data/test.py
+++ AI model/data/test.py
@@ -26,6 +26,18 @@ X_train, X_test, y_train, y_test = train_test_split(frames_arr,angles_arr,test_s
 
 #plt.scatter(frames_arr,angles_arr)
 #plt.show()
+arr2New = np.genfromtxt("wrongForm.csv", delimiter=",")
+frames_arrNew = arr2New[:,3] #all rows in colm 1 
+angles_arrNew = arr2New[:,2] #all rows in colm 1 
+
+X_train2, X_test2, y_train2, y_test2 = train_test_split(frames_arrNew,angles_arrNew,test_size=0.3)
+x_train_data2 = np.array([X_train2]).reshape(-1,1)
+#y_train_data = np.array([y_train]).reshape(-1,1)
+x_test_data2 = np.array([X_test2]).reshape(-1,1)
+#y_test_data = np.array([y_test]).reshape(-1,1)
+
+
+
 
 
 x_train_data = np.array([X_train]).reshape(-1,1)
@@ -62,17 +74,25 @@ rs = RandomizedSearchCV(model, n_iter=10,
 
 #print(rs.best_params_)
 '''
+from sklearn.model_selection import cross_val_score
 
 rf = RandomForestRegressor(n_estimators=100, max_depth=6,
-                           min_samples_split=28, random_state=1)
+                           min_samples_split=28, random_state=3)
 
 rf.fit(x_train_data, y_train)
 
 pred_y_data = rf.predict(x_test_data)
+pred_y_data2 = rf.predict(x_test_data2)
 
 plt.scatter(x_train_data,y_train)
-plt.scatter(x_test_data,pred_y_data)
-#print(rs.score(x_test_data,pred_y_data))
+plt.scatter(x_test_data2,pred_y_data2)
+
+
+
+scores = cross_val_score(estimator=rf, X=x_test_data, y=pred_y_data, cv=10,)
+print(scores)
+
+print(rf.score(x_test_data,pred_y_data))
 #print(mean_absolute_error(y_test, pred_y_data))
 #print(accuracy_score(y_test,pred_y_data))
 
--- AI model/nonLinearModel.py
+++ AI model/nonLinearModel.py
@@ -45,49 +45,105 @@ Xtrain, Xtest, yTrain, yTest = train_test_split(TrainingSet[:,1:2],TrainingSet[:
 #     tf.keras.layers.Dense(64, activation="relu", input_shape=(1,), kernel_initializer="uniform"),
 #     tf.keras.layers.Dense(64, activation="relu", kernel_initializer="uniform"),
 #     tf.keras.layers.Dense(128, activation="softmax", kernel_initializer="uniform")
-# ])
-from keras.layers import Dense
-from keras.models import Sequential
+# # ])
+# from keras.layers import Dense
+# from keras.models import Sequential
 
-model = Sequential()
-model.add(Dense(2, activation="tanh", input_dim=1, ))
-model.add(Dense(1, activation="linear",))
-
-
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-
-print(Xtrain.shape)
-print(yTrain.shape)
-
-# Fit the model
-model.fit(Xtrain, yTrain, epochs=200, batch_size=1,  verbose=2)
-
-# Calculate predictions
-PredTestSet = model.predict(Xtest)
-# PredValSet = model.predict(Xtest)
-
-# Save predictions
-np.savetxt("trainresults.csv", PredTestSet, delimiter=",")
-# np.savetxt("valresults.csv", PredValSet, delimiter=",")
+# model = Sequential()
+# model.add(Dense(2, activation="tanh", input_dim=1, ))
+# model.add(Dense(1, activation="linear",))
 
 
-#Plot actual vs predition for training set
-TestResults = np.genfromtxt("trainresults.csv", delimiter=",")
+# model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
-plt.plot(yTrain,TestResults,'ro')
-plt.title('Training Set')
-plt.xlabel('Actual')
-plt.ylabel('Predicted')
+# print(Xtrain.shape)
+# print(yTrain.shape)
 
-#Compute R-Square value for training set
-TestR2Value = r2_score(yTrain,TestResults)
-print("Training Set R-Square=", TestR2Value)
+# # Fit the model
+# model.fit(Xtrain, yTrain, epochs=200, batch_size=1,  verbose=2)
+
+# # Calculate predictions
+# PredTestSet = model.predict(Xtest)
+# # PredValSet = model.predict(Xtest)
+
+# # Save predictions
+# np.savetxt("trainresults.csv", PredTestSet, delimiter=",")
+# # np.savetxt("valresults.csv", PredValSet, delimiter=",")
 
 
+# #Plot actual vs predition for training set
+# TestResults = np.genfromtxt("trainresults.csv", delimiter=",")
+
+# plt.plot(yTrain,TestResults,'ro')
+# plt.title('Training Set')
+# plt.xlabel('Actual')
+# plt.ylabel('Predicted')
+
+# #Compute R-Square value for training set
+# TestR2Value = r2_score(yTrain,TestResults)
+# print("Training Set R-Square=", TestR2Value)
 
 
 
 
+
+
+
+
+
+# # Import required libraries
+# import tensorflow as tf
+# import numpy as np
+
+# data = pd.read_csv('courses.csv')
+
+# videos = []
+# temp = []
+# for x in range(0,len(data)):
+#     temp.append(data['1'][x])
+
+#     if(data['0'][x] == 0):
+#         videos.append(temp)
+#         temp = []
+#         print(data['1'][x])
+# data = pd.read_csv('courses.csv')
+# # Collect dataset of correct rep graphs
+# rep_graphs = ...
+
+# # Build model using TensorFlow
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Dense(64, activation='relu'))
+# model.add(tf.keras.layers.Dense(64, activation='relu'))
+# model.add(tf.keras.layers.Dense(1))
+
+# # Compile model
+# model.compile(optimizer='adam', loss='mse')
+
+# # Train model on dataset of correct rep graphs
+# for rep_graph in rep_graphs:
+#   # Forward pass
+#   ideal_formula = model(rep_graph)
+
+#   # Compute loss
+#   loss = tf.keras.losses.mse(ideal_formula, rep_graph)
+
+#   # Backward pass
+#   gradients = tape.gradient(loss, model.trainable_variables)
+#   optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+# # Use trained model to generate ideal formula for a given exercise
+# exercise_rep_graph = ...
+# ideal_formula = model(exercise_rep_graph)
+
+# # Evaluate model's performance on testing dataset
+# testing_rep_graphs = ...
+# scores = []
+# for rep_graph in testing_rep_graphs:
+#   ideal_formula = model(rep_graph)
+#   score = calculate_score(ideal_formula, rep_graph)
+#   scores.append(score)
+
+# mean_score = np.mean(scores)
 
 
 
@@ -95,8 +151,11 @@ print("Training Set R-Square=", TestR2Value)
 import tensorflow as tf
 import numpy as np
 
-# Collect dataset of correct rep graphs
-rep_graphs = ...
+Xtrain, Xtest, yTrain, yTest = train_test_split(TrainingSet[:,1:2],TrainingSet[:,2], test_size = .8, random_state = 0)
+
+# Preprocess data
+frames = Xtrain
+angles = yTrain
 
 # Build model using TensorFlow
 model = tf.keras.models.Sequential()
@@ -107,29 +166,40 @@ model.add(tf.keras.layers.Dense(1))
 # Compile model
 model.compile(optimizer='adam', loss='mse')
 
-# Train model on dataset of correct rep graphs
-for rep_graph in rep_graphs:
+# Train model on input data
+for frames, angles in zip(frames, angles):
   # Forward pass
-  ideal_formula = model(rep_graph)
+  ideal_formula = model(frames, angles)
 
   # Compute loss
-  loss = tf.keras.losses.mse(ideal_formula, rep_graph)
+  loss = tf.keras.losses.mse(ideal_formula, angles)
 
   # Backward pass
-  gradients = tape.gradient(loss, model.trainable_variables)
-  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+  gradients = tf.GradientTape(loss, model.trainable_variables)
+  tf.keras.optimizers.Optimizer(zip(gradients, model.trainable_variables))
 
+
+
+def calculate_mse(generated, correct):
+  # Compute the difference between the generated and correct rep graphs
+  difference = generated - correct
+
+  # Square the difference
+  squared_difference = difference ** 2
+
+  # Take the mean of the squared difference
+  mse = np.mean(squared_difference)
+
+  return mse
 # Use trained model to generate ideal formula for a given exercise
-exercise_rep_graph = ...
-ideal_formula = model(exercise_rep_graph)
+exercise_frames = ...
+exercise_angles = ...
+ideal_formula = model(exercise_frames, exercise_angles)
 
 # Evaluate model's performance on testing dataset
-testing_rep_graphs = ...
+testing_frames = ...
+testing_angles = ...
 scores = []
-for rep_graph in testing_rep_graphs:
-  ideal_formula = model(rep_graph)
-  score = calculate_score(ideal_formula, rep_graph)
-  scores.append(score)
-
-mean_score = np.mean(scores)
-
+for frames, angles in zip(testing_frames, testing_angles):
+  ideal_formula = model(frames, angles)
+  score = 0
