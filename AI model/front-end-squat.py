dd2ebec133e952bfc845edb2bf9bda4c1531efbc
Nipuna <71096857+nipunaprs@users.noreply.github.com>
2023-02-27 16:21:04 -0500

    squat reccomendations + exercise choice
    - squat depth reccomendation implemented
    - knee angle imbalance implemented

--- AI model/front-end-squat.py
+++ AI model/front-end-squat.py
@@ -25,6 +25,11 @@ max_right = 0
 lowest_wrist_distance = 120
 imbalancefound = False
 
+#Reccomendation code max angles squat
+imbalancefoundsquat = False
+lowest_feet_distance = 120
+min_depth = 40
+
 switch = 0
 dataAngles = [[[],[]],[[],[]]]
 angles = []
@@ -243,7 +248,7 @@ def bench_recs(image, keypoints):
         img_num = 1
         print("Angle imbalance detected",left_shoulder_angle,right_shoulder_angle)
         print("RECCOMENDATION: Try lowering the weight as you're form is showing an imbalance")
-        cv2.imwrite("imbalance{}.jpg".format(img_num),image) #This is to prevent over-writes but also can use just reg string
+        cv2.imwrite("imbalance_bench{}.jpg".format(img_num),image) #This is to prevent over-writes but also can use just reg string
         imbalancefound = True
         img_num += 1
 
@@ -265,22 +270,83 @@ def bench_recs(image, keypoints):
         img_numv2 += 1
         #print(distance_of_wrists, " ", lowest_wrist_distance)
 
-def check_reccs():
-    global max_left
-    global max_right
-    global imbalancefound
-    global lowest_wrist_distance
+def squat_recs(image, keypoints):
 
-    if(max_right < 150 or max_left < 150):
-        print("RECCOMENDATION: You need to lift the bar all the way up -- Try lower the weight as your max angles were: ", max_left, max_right)
-    else:
-        print("OBSERVATION: You're lifting the bar all the way to the top! Good job!",max_right,max_right)
+    global min_depth
+    global lowest_feet_distance
+    global imbalancefoundsquat
 
-    if(imbalancefound == False):
-        print("OBSERVATION: No imbalance in arms found")
+    #CALCULATE LEFT SHOULDER ANGLE
+    left_knee_angle = calculate_angle(keypoints[11],keypoints[13],keypoints[15])
+    #CALCULATE RIGHT SHOULDER ANGLE
+    right_knee_angle = calculate_angle(keypoints[12],keypoints[14],keypoints[16])
+
+    confidence_factor =  (keypoints[14][2] + keypoints[13][2]) / 2
+
+    #Recomendation 1 -- Imbalance in knees
+        #Found based on difference between left and right angle past a certain threshold (in this case 45)
+    #print(abs(left_knee_angle-right_knee_angle))    
+    if(abs(left_knee_angle-right_knee_angle) >= 35 and confidence_factor > 0.5):
+        img_num = 1
+        print("Angle imbalance detected",left_knee_angle,right_knee_angle)
+        #print("RECCOMENDATION: You're knee's are imbalanced, try to re-center yourself or lower you're weight to go down equally")
+        cv2.imwrite("imbalance_squat{}.jpg".format(img_num),image) #This is to prevent over-writes but also can use just reg string
+        imbalancefoundsquat = True
+        img_num += 1
     
-    if(lowest_wrist_distance < 120):
-        print("RECCOMENDATION: Try to move you're arms further apart - They might be too close together")
+    
+    #Recomendation 2 -- Feet too close
+        #Found based on difference between two feet
+    #distance_of_feet = abs(keypoints[16][1] - keypoints[15][1])
+    
+    
+    #Recomendation 3 -- Squat Depth
+        #CALCULATE RIGHT HIP - RIGHT ANKLE
+    distance_of_body = abs(keypoints[12][1] - keypoints[16][1])
+    
+    if distance_of_body < min_depth:
+        img_numv2 = 1
+        cv2.imwrite("lowest_depth{}.jpg".format(img_numv2),image)
+        min_depth = distance_of_body
+        img_numv2 += 1
+        #print(distance_of_wrists, " ", lowest_wrist_distance)
+    
+    
+
+def check_reccs():
+    global choice
+
+    #BENCH
+    if choice == 0:
+        global max_left
+        global max_right
+        global imbalancefound
+        global lowest_wrist_distance
+
+        if(max_right < 150 or max_left < 150):
+            print("RECCOMENDATION: You need to lift the bar all the way up -- Try lower the weight as your max angles were: ", max_left, max_right)
+        else:
+            print("OBSERVATION: You're lifting the bar all the way to the top! Good job!",max_right,max_right)
+
+        if(imbalancefound == False):
+            print("OBSERVATION: No imbalance in arms found")
+        
+        if(lowest_wrist_distance < 120):
+            print("RECCOMENDATION: Try to move you're arms further apart - They might be too close together")
+    #SQUAT
+    elif choice == 1:
+        
+        global min_depth
+        global imbalancefoundsquat
+        global lowest_feet_distance
+
+        if(imbalancefoundsquat == False):
+            print("OBSERVATION: No imbalance in knees found")
+        
+        if(min_depth > 25):
+            print("RECCOMENDATION: You're likely not going deep enough. Try to go deeper with you're squat to maximize tension. This may require a lower weight. Lowest Depth Recorded: " + str(min_depth))
+        else:
+            print("OBSERVATION: You are going to optimal depth with you're squats! Lowest Depth Recorded: " + str(min_depth))
 
 
 def main(vidPath):
@@ -326,6 +392,11 @@ def main(vidPath):
 # ---------------------INPUTS--------------------------------------
 print("-------AI GYM ASSISTANT V1.0---------")
 
+print("--OPTIONS--\n")
+print("Bench - 0\n")
+print("Squats - 1\n")
+choice = int(input("Please enter which type of exercise you want to analyze \n"))
+
 filename = input("Please enter ur file name with extension (ex: user.mp4) \n")
 
 live_mode = input("Please enter if you want live mode (Y/N) \n")
@@ -342,6 +413,7 @@ print(vidPath)
 
 
 def main(vidPath):
+    global choice
 
     cap = cv2.VideoCapture(vidPath)
     fname = 'op_' + str(vidPath.split("/")[-1])
@@ -373,8 +445,11 @@ def main(vidPath):
             cv2.imshow("frame", outimage)
         
         #------Reccomendations code---------
-        #bench_recs(image, curr_kp)
-        
+
+        if choice == 0:
+            bench_recs(image, curr_kp)
+        elif choice == 1:
+            squat_recs(image,curr_kp)
 
 
         #------Reccomendations code end---------
@@ -383,7 +458,7 @@ def main(vidPath):
         if k == ord('q') or k == 27:
             break
     
-    #check_reccs() #Checks any reccomendations after entire video played
+    check_reccs() #Checks any reccomendations after entire video played
 
     cap.release()
     cv2.destroyAllWindows()
@@ -466,7 +541,11 @@ plt.show()
 
 
 # ---------------------FEED INTO MODEL-------------------------------------
-rf = joblib.load("squat-model.joblib")
+rf = None
+if choice == 0:
+    rf = joblib.load("model.joblib") #Double check this is correct
+elif choice == 1:
+    rf = joblib.load("squat-model.joblib")
 
 #Getting one random test video
 
