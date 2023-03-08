ee5a0135b97e184b8093e8535cca35a6bc3edcf1
vchauha4 <vchauha4@uwo.ca>
2023-03-08 16:40:13 -0500

    getFileNames

---
+++ AI model/getFileNames.py
@@ -0,0 +1,13 @@
+import os
+# assign directory
+directory = 'data\\wrong-front'
+stingLIst=[]
+# iterate over files in
+# that directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        stingLIst.append(str(f))
+        # print(filename)
+print(stingLIst)
